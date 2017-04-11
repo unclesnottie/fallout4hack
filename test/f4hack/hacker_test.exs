4bc06d2e54f4e1a9733292a2f9a113370c70eb1a
Scott Keller <scott.keller13@gmail.com>
2017-04-11 13:30:35 -0400

    Group tests using describe blocks

--- test/f4hack/hacker_test.exs
+++ test/f4hack/hacker_test.exs
@@ -4,60 +4,66 @@ defmodule F4Hack.HackerTest do
 
   alias F4Hack.Hacker
 
-  test "process_input splits into list of lists" do
-    expected = [["H","E","L","L","O"], ["F","O","O","L","S"], ["B","O","O","K","S"]]
-    actual = "HELLO FOOLS BOOKS"
-      |> Hacker.process_input
-    assert actual == expected
+  describe "Hacker.process_input/1" do
+    test "splits into list of lists" do
+      expected = [["H","E","L","L","O"], ["F","O","O","L","S"], ["B","O","O","K","S"]]
+      actual = "HELLO FOOLS BOOKS"
+        |> Hacker.process_input
+      assert actual == expected
+    end
+
+    test "uppercases each letter" do
+      actual ="hello FOOLS bOOkS"
+        |> Hacker.process_input
+        |> List.flatten
+        |> Enum.join
+      expected = String.upcase(actual)
+      assert actual == expected
+    end
   end
 
-  test "process_input uppercases each letter" do
-    actual ="hello FOOLS bOOkS"
-      |> Hacker.process_input
-      |> List.flatten
-      |> Enum.join
-    expected = String.upcase(actual)
-    assert actual == expected
+  describe "Hacker.calculate_likenesses/2" do
+    test "removes the word itself" do
+      words = [["B","O","O","K","S"]]
+      word = ["B","O","O","K","S"]
+      expected = {"BOOKS", []}
+      actual = Hacker.calculate_likenesses(word, words)
+      assert actual == expected
+    end
+
+    test "calculates correctly when same letters are in different places" do
+      words = [["P","O","R","T","S"]]
+      word = ["S","P","O","R","T"]
+      expected = {"SPORT", [{"PORTS", 0}]}
+      actual = Hacker.calculate_likenesses(word, words)
+      assert actual == expected
+    end
+
+    test "calculates likeness correctly" do
+      words = [["B","O","O","K","S"], ["F","O","O","L","S"], ["E","M","A","I","L"], ["S","T","O","C","K"]]
+      word = ["B","O","O","K","S"]
+      expected = {"BOOKS", [{"FOOLS", 3}, {"EMAIL", 0}, {"STOCK", 1}]}
+      actual = Hacker.calculate_likenesses(word, words)
+      assert actual == expected
+    end
   end
 
-  test "calculate_likenesses removes the word itself" do
-    words = [["B","O","O","K","S"]]
-    word = ["B","O","O","K","S"]
-    expected = {"BOOKS", []}
-    actual = Hacker.calculate_likenesses(word, words)
-    assert actual == expected
-  end
+  describe "Hacker.calculate_likeness/2" do
+    test "returns zero when no letters match" do
+      assert Hacker.calculate_likeness(["M","A","I","N"], ["O","B","O","E"]) == 0
+    end
 
-  test "calculate_likenesses calculates correctly when same letters are in different places" do
-    words = [["P","O","R","T","S"]]
-    word = ["S","P","O","R","T"]
-    expected = {"SPORT", [{"PORTS", 0}]}
-    actual = Hacker.calculate_likenesses(word, words)
-    assert actual == expected
-  end
+    test "returns zero when letters match but are out of order" do
+      assert Hacker.calculate_likeness(["P","O","R","T","S"], ["S","P","O","R","T"]) == 0
+    end
 
-  test "calculate_likenesses calculates likeness correctly" do
-    words = [["B","O","O","K","S"], ["F","O","O","L","S"], ["E","M","A","I","L"], ["S","T","O","C","K"]]
-    word = ["B","O","O","K","S"]
-    expected = {"BOOKS", [{"FOOLS", 3}, {"EMAIL", 0}, {"STOCK", 1}]}
-    actual = Hacker.calculate_likenesses(word, words)
-    assert actual == expected
-  end
+    test "returns 1 when a letter matches" do
+      assert Hacker.calculate_likeness(["B","A","R","N"], ["B","O","O","K"]) == 1
+    end
 
-  test "calculate_likeness returns zero when no letters match" do
-    assert Hacker.calculate_likeness(["M","A","I","N"], ["O","B","O","E"]) == 0
-  end
-
-  test "calculate_likeness returns zero when letters match but are out of order" do
-    assert Hacker.calculate_likeness(["P","O","R","T","S"], ["S","P","O","R","T"]) == 0
-  end
-
-  test "calculate_likeness returns 1 when a letter matches" do
-    assert Hacker.calculate_likeness(["B","A","R","N"], ["B","O","O","K"]) == 1
-  end
-
-  test "calculate_likeness returns correct count when more than one letters match" do
-    assert Hacker.calculate_likeness(["B","O","O","K","S"], ["F","O","O","L","S"]) == 3
+    test "returns correct count when more than one letters match" do
+      assert Hacker.calculate_likeness(["B","O","O","K","S"], ["F","O","O","L","S"]) == 3
+    end
   end
 
 end
