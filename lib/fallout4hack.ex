93a00a80030d278a5a5b3ab943e040f7add3db04
Scott Keller <scott.keller13@gmail.com>
2017-11-20 14:56:56 -0500

    Remove commented out code

--- lib/fallout4hack.ex
+++ lib/fallout4hack.ex
@@ -35,39 +35,6 @@ defp hack_password(pid) do
   end
 end
 
-  # def main(_args) do
-  #   likenesses = IO.gets("Enter possible passwords: ")
-  #     |> Hacker.process_input
-  #     |> Hacker.calculate_all_likenesses
-  #     |> do_work
-  # end
-
-  # defp do_work(likenesses) do
-  #   best_guess = Hacker.make_best_guess(likenesses)
-  #
-  #   IO.puts "You're best guess is \"#{best_guess}\"."
-  #   likeness = IO.gets("Enter likeness: ")
-  #     |> to_int
-  #
-  #   if likeness == String.length(best_guess) do
-  #     IO.puts("The password is \"#{best_guess}\"!")
-  #     exit :normal
-  #   end
-  #
-  #   remaining_likenesses = Hacker.make_guess(likenesses, best_guess)
-  #
-  #   remaining_words = Hacker.get_remaining_words(remaining_likenesses, likeness)
-  #
-  #   if String.length(best_guess) == String.length(remaining_words) do
-  #     IO.puts("The password is \"#{remaining_words}\"!")
-  #     exit :normal
-  #   end
-  #
-  #   IO.puts ["Remaining words: ", remaining_words, "\n"]
-  #
-  #   do_work(remaining_likenesses[likeness])
-  # end
-
   defp to_int(int) do
     int
       |> String.trim
